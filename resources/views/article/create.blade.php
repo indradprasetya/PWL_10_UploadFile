ba3e931635779be1628f7673f1485d8138c95cd8
indradprasetya <indrayanadian7@gmail.com>
2023-06-19 08:14:34 +0700

    edit create view

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -5,11 +5,11 @@
             @csrf
             <div class="form-group">
                 <label for="title">Title: </label>
-                <input type="text" class="form-control" required="required" name="title"></br>
+                <input type="text" class="form-control" required="required" name="title"><br>
                 <label for="content">Content: </label>
-                <textarea type="text" class="form-control" required="required" name="content"></textarea></br>
+                <textarea type="text" class="form-control" required="required" name="content"></textarea><br>
                 <label for="image">Feature Image: </label>
-                <input type="file" class="form-control" required="required" name="image"></br>
+                <input type="file" class="form-control" required="required" name="image"><br>
                 <button type="submit" name="submit" class="btn btn-primary float-right">Simpan</button>
             </div>
         </form>
